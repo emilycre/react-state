696b1aed07fcb245f9cd77caf75ad683e6a13080
Emily Creamer <emily.creamer@icloud.com>
2019-05-14 12:52:59 -0700

    Incrementer tests passing

---
+++ src/components/Incrementer.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Incrementer from './Incrementer';
+
+describe('Incrementer component', () => {
+  it('Renders an incrementer', () => {
+    const wrapper = shallow(<Incrementer />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+  it('Incremembers a count on button click', () => {
+    const wrapper = shallow(<Incrementer />);
+
+    wrapper.find('button').at(0).simulate('click');
+    expect(wrapper.state('count')).toEqual(1);
+  });
+});
