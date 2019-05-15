22536839b952e9a1fcd0188239f0adcf8724c306
Emily Creamer <emily.creamer@icloud.com>
2019-05-14 17:17:49 -0700

    changes color, test passing

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -7,4 +7,11 @@ describe('App component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('Changes color on selectColor event', () => {
+    const wrapper = shallow(<App />);
+
+    wrapper.instance().selectColor('green');
+    expect(wrapper.state('color')).toEqual('green');
+  });
 });
